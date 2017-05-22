6da99574e292ff6e48dc974b4e86136f14b79930
Rebecca <Rebecca@172.20.94.17>
2017-05-22 08:43:00 +0200

    countryborders

--- src/it/polito/tdp/db/DBConnect.java
+++ src/it/polito/tdp/db/DBConnect.java
@@ -4,14 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.DataSources;
+
 public class DBConnect {
 	
 	private static String jdbcURL = "jdbc:mysql://localhost/countries?user=root&password=root" ;
+	private static DataSource ds;
 	
 	public static Connection getConnection() {
 		
+		if(ds==null) {
+			try {
+				ds = DataSources.pooledDataSource(
+						DataSources.unpooledDataSource(jdbcURL));
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		try {
-			Connection c = DriverManager.getConnection(jdbcURL) ;
+			Connection c = ds.getConnection() ;
 			return c ;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
